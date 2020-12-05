d2cb156e0e121abc1e66b31b31b370edb66b1ed8
Lockett Pundt <kalifornium@hotmail.com>
2020-12-05 15:36:35 -0500

    refactor(Advent Of Code): :fire: refactored into single function

--- day5/day5.js
+++ day5/day5.js
@@ -1,43 +1,8 @@
 const data = require('fs').readFileSync('input.txt', 'utf-8').split('\n');
 const testData = require('fs').readFileSync('testInput.txt', 'utf-8').split('\n')
 
-// part one...
-const binaryBoarding = (passes) => {
-  return Math.max(...passes.map(x => {
-    const { row, column } = x.split('').reduce((obj, letter) => {
-      switch (letter) {
-        case 'F':
-          obj.row.upper = Math.floor((obj.row.upper + obj.row.lower) / 2)
-          break
-        case 'B':
-          obj.row.lower = Math.ceil((obj.row.upper + obj.row.lower) / 2)
-          break
-        case 'L':
-          obj.column.upper = Math.floor((obj.column.upper + obj.column.lower) / 2)
-          break
-        case 'R':
-          obj.column.lower = Math.floor((obj.column.upper + obj.column.lower) / 2)
-          break
-      }
-      return obj
-    }, {
-      row: {
-        lower: 0,
-        upper: 127,
-      },
-      column: {
-        lower: 0,
-        upper: 7,
-      }
-    })
-    return row.upper * 8 + column.upper
-  })
-  )
-}
-
-// part two...
 const binaryBoardingTwo = (passes) => {
-  const [mySeat,] = passes.map(x => {
+  const seatIds = passes.map(x => {
     const { row, column } = x.split('').reduce((obj, letter) => {
       switch (letter) {
         case 'F':
@@ -66,9 +31,9 @@ const binaryBoardingTwo = (passes) => {
     })
     return row.upper * 8 + column.upper
   })
-  .sort((a, b) => a - b)
-  .filter((x, i, arr) => +arr[i + 1] !== +x + 1)
-  return mySeat
+  const [ mySeat ] = seatIds.sort((a, b) => a - b)
+    .filter((x, i, arr) => +arr[i + 1] && +arr[i + 1] !== +x + 1)
+  return `The highest seat Id is ${seatIds[seatIds.length - 1]} and your seat Id is ${mySeat + 1}.}`
 }
 
-console.log(JSON.stringify(binaryBoardingTwo(data), null, 1))
+console.log(binaryBoardingTwo(data))
