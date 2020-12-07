f89797d71c3caac10a4ab278f0be00cad0f1482b
Lockett Pundt <kalifornium@hotmail.com>
2020-12-06 20:10:15 -0500

    feat(Advent Of Code):

--- day6/day6.js
+++ day6/day6.js
@@ -1,8 +1,24 @@
 const data = require('fs').readFileSync('input.txt', 'utf-8');
 const testData = require('fs').readFileSync('testInput.txt', 'utf-8');
-const parsedInput = data.split(/\n\s/g).map(x => x.replace(/\n/g, ''))
+const parsedInput = data.split(/\n\s/g)
 
+// part one...
 const customDeclarationForm = (forms) => {
-  return forms.reduce((total, x) => total += [...new Set(x)].length, 0)
+  return forms.map(x => x.replace(/\n/g, '')).reduce((total, x) => total += [...new Set(x)].length, 0)
 }
-console.log(customDeclarationForm(parsedInput))
+
+// console.log(customDeclarationForm(parsedInput))
+
+// part two...
+const customDeclarationFormTwo = (forms) => {
+  return forms
+    .map(x => x.replace(/\n/g, ' '))
+    .reduce((total, x) => {
+      const people = x.split(' ').map(question => question.split(''))
+      const totalOfYes = people.map(x => x.filter((q) => people.every(person => person.includes(q))))
+      return [...total, ...new Set(totalOfYes.flat())];
+    }, [])
+    .reduce((sum, x) => sum += x.length, 0)
+}
+
+console.log(customDeclarationFormTwo(parsedInput))
