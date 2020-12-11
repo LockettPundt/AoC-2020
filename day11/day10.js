aa42d57226f1327491004b71834d674a83434b5c
Lockett Pundt <kalifornium@hotmail.com>
2020-12-11 17:08:05 -0500

    feat(Advent Of Code): :rocket: Day 11, solution 1...

---
+++ day11/day10.js
@@ -0,0 +1,37 @@
+const data = require('fs').readFileSync('input.txt', 'utf-8').replace(/[L]/g, '#').split('\n')
+const testData = require('fs').readFileSync('testInput.txt', 'utf-8').replace(/[L]/g, '#').split('\n')
+
+const seatingSystem = (data) => {
+  let result = [...data]
+  for (;;) {
+    let temp = [...result].map((line, i) => {
+      return line.split('').map((x, index) => {
+        if (x === '.') return x
+        const occupiedSeats = [
+          result[i - 1] && result[i - 1][index - 1] === '#',
+          result[i - 1] && result[i - 1][index] === '#',
+          result[i - 1] && result[i - 1][index + 1] === '#',
+          result[i][index - 1] === '#',
+          result[i][index + 1] === '#',
+          result[i + 1] && result[i + 1][index - 1] === '#',
+          result[i + 1] && result[i + 1][index] === '#',
+          result[i + 1] && result[i + 1][index + 1] === '#',
+        ].filter(bool => !!bool)
+        if (occupiedSeats.length >= 4 && x === '#') {
+          return 'L'
+        } else if (x === 'L' && !occupiedSeats.length) {
+          return '#'
+        }
+        return x
+      }).join('')
+    })
+    if (temp.every((x, i) => x === result[i])) {
+      break
+    } else {
+      result = temp
+    }
+  }
+  return result.reduce((sum, x) => sum += x.split('').filter(seat => seat === '#').length, 0)
+}
+
+console.log(seatingSystem(data))
